985beb29d8ff103272c8106c01f336e124b6fab1
Jan Van Sweevelt <vansweej@gmail.com>
2022-07-17 21:55:30 +0200

    added neovim

---
+++ run_once_install_nvim.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env sh
+
+brew install neovim
+
+sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
+       https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
+
+sleep .5
+
+nvim +PlugInstall +qall
+
+cd $HOME/.local/share/nvim/plugged/telescope-fzf-native.nvim && make clean && make
